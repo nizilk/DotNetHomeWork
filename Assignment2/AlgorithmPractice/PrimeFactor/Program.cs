06ab0127c1d54ef425cef8149b710957182bf4ec
nizilk <66125871+nizilk@users.noreply.github.com>
2023-03-04 01:39:22 +0800

    WEEK1_Assignment2_Project1
    输出给定数的所有素数因子

---
+++ Assignment2/AlgorithmPractice/PrimeFactor/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+
+namespace PrimeFactor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            bool flag;
+            Console.Write("Please input an int number: ");
+            flag = Int32.TryParse(Console.ReadLine(), out num);
+            Console.WriteLine();
+
+            if (flag)
+            {
+                ArrayList ans = PrimeFac(num);
+                if(ans.Count == 0)
+                {
+                    Console.Write("None! ");
+                }
+                foreach (int i in ans)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+            else
+            {
+                Console.Write("Error! ");
+            }
+            Console.Write("\n\nPress any to exit...");
+            Console.ReadKey(true);
+        }
+
+        static ArrayList PrimeFac(int num)
+        {
+            ArrayList ans = new ArrayList();
+            for(int i = 2; i <= num; i++)
+            {
+                if(num%i != 0)
+                {
+                    continue;
+                }
+                if(IsPrime(i))
+                {
+                    ans.Add(i);
+                }
+            }
+            return ans;
+        }
+
+        static bool IsPrime(int n)
+        {
+            for(int i = 2; i < n; i++)
+            {
+                if(n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
