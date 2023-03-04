666f40742c45f8ed22df983db5c740916a289709
nizilk <66125871+nizilk@users.noreply.github.com>
2023-03-04 19:49:06 +0800

    Proj3&4

---
+++ Assignment2/AlgorithmPractice/EraSieve/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Data.SqlTypes;
+
+namespace EraSieve
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ans = Era(100);
+            foreach (int i in ans)
+            {
+                Console.Write($"{i}  ");
+            }
+            Console.Write($"\n\nPress any key to exit...");
+            Console.ReadKey(true);
+        }
+
+        static int[] Era(int num)
+        {
+            int[] a = new int[num + 1];     //下标代表数，对应值为0则是素数，为1则不是素数
+            a[0] = a[1] = 1;
+            int count = num - 1, k=0, i;
+            int it = (int)Math.Sqrt(num);
+            for (i=2; i<=it; i++)
+            {
+                if (a[i] == 1)
+                {
+                    continue;
+                }
+                for(int j=i*i; j<=num; j += i)
+                {
+                    if (a[j] == 1) { continue; }
+                    a[j] = 1;
+                    count--;
+                }
+            }
+            int[] res = new int[count];
+            for (i=0; i<=num; i++)
+            {
+                if (a[i] == 0)
+                {
+                    res[k] = i;
+                    k++;
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ Assignment2/AlgorithmPractice/ToeplitzMatrix/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Runtime.InteropServices;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ToeplitzMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int m, n;
+            string[] row;
+            Console.WriteLine("Please input the size of matrix: ");
+            Console.Write("M = ");
+            int.TryParse(Console.ReadLine(), out m);
+            Console.Write("N = ");
+            int.TryParse(Console.ReadLine(), out n);
+            double[,] matrix = new double[m, n];
+            Console.WriteLine("\nPlease input a matrix: ");
+            for(int i=0; i<m; i++)
+            {
+                row = Console.ReadLine().Split(new string[] { " ", ",", "，", ";", "；" }, StringSplitOptions.RemoveEmptyEntries);
+                for (int j=0; j<n; j++)
+                {
+                    matrix[i, j] = Double.Parse(row[j]);
+                }
+            }
+            bool ans = IsTMatrix(matrix);
+            Console.WriteLine($"\n{ans}");
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+
+        static bool IsTMatrix(double[,] a)
+        {
+            for(int i=1; i<a.GetLength(0); i++)
+            {
+                for(int j=1; j<a.GetLength(1); j++)
+                {
+                    if (a[i,j] != a[i-1,j-1]) return false;
+                }
+            }
+            return true;
+        }
+    }
+}
